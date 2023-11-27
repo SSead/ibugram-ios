84bcd1b1b51edf5c06dbd0821198b67d2a0c633f
SSead <smailagicsead@gmail.com>
2023-11-27 14:21:53 +0100

    Initial Commit

---
+++ ibugram/ibugramApp.swift
@@ -0,0 +1,17 @@
+//
+//  ibugramApp.swift
+//  ibugram
+//
+//  Created by Sead Smailagic on 27. 11. 2023..
+//
+
+import SwiftUI
+
+@main
+struct ibugramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
